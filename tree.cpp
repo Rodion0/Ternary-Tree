b2c8267d2e61448d17ebf4b5e55a02dcadaa2846
Rodion0 <jtham3000@gmail.com>
2021-03-08 20:02:30 -0600

    03/08/21

--- tree.cpp
+++ tree.cpp
@@ -6,10 +6,21 @@
 // Description: Ternary Trees 
 // Assistance: We'll see 
 //--------------------------------------------------------------------
-//Import Strings, limits, iostream 
+
+
+//Current Problems
+    //Insert Method 
+        //For some reason doesnt want to add to another cell or go down tree
+    //Print 
+        //I hope to Jesus, Mary and Joseph it works
+    //Main 
+        //Fuck it looks right 
+
+
+
+//Import limits, iostream 
 #include <iostream>
-#include <limits>
-#include <string>
+#include <climits>
 using namespace std; 
 
 //Node Class
@@ -28,15 +39,21 @@ public:
 };
 
 node::node(){
+    cout << "I have made it to the node" << endl; 
     small = INT_MAX; 
     large = INT_MIN; 
-    left = right = middle = NULL;
+    left = NULL;
+    right = NULL;
+    middle = NULL;
 }
+
 node::node(int value)
 {
     small = value;
     large = INT_MIN;
-    left = right = middle = NULL; 
+    left = NULL;
+    right = NULL;
+    middle = NULL; 
 }
  
 //Tree Class
@@ -63,43 +80,59 @@ node * tree::getRoot(){
 //Insert Method
 node * tree::insert(node * current, int value){
     //Base Case 
-    if(root == NULL){
-        return new node(value);
+    if(current == NULL){
+        cout << "I have returned" << endl;  
+        current = new node(value);
+        if(root == NULL){
+            cout << "Root here" <<endl;
+            root = new node(value);
+            return root;
+        }
+        return current;  
     }
     else{
         // Check to see if node is filled
         if (current->large == INT_MIN)
         {
             if(value < current->small){
+                cout << "Swapping" << endl; 
                 current->large = current->small; 
                 current->small = value;  
             }
             else{
+                cout << "Doesn't need to be swapped" <<endl; 
                 current->large = value;
             }
         }
-        else if(value < current->small){
+        else if(value <= current->small){
             if(current->left == NULL){
+                cout << "Small boi" << endl; 
                 current->left = current;
             }
             else{
+                cout << "Small Boi insert" <<endl; 
                 insert(current->left,value);
             }
         }
         else if(value > current->small && value < current->large){
-            if(current-> middle== NULL){
+            if(current-> middle == NULL){
+                cout <<"Middle Boy" <<endl;
                 current->middle = current;
             }
             else{
+                cout << "Middle Boy INsert" << endl; 
                 insert(current->middle,value);
             }
         }
         else if(value > current->large){
             if(current->right == NULL){
+                cout << "Large Boy" << endl; 
                 current->right = current;
             }
             else{
+                cout << "Large  Boy Insert" <<endl; 
                 insert(current->right,value);
+                cout << "Hello Darkness my old friend" <<endl; 
             }
         }
         return current; 
@@ -159,10 +192,9 @@ void tree::print(node * current) // In-order traversal
 int main(int argc, char const *argv[])
 {
     // New Tree
-    tree * ternary = new tree(); 
+    tree * ternary = new tree();
     //Input numberOfInts from Parameters
-    int numberOfInts = stoi(argv[1]), first = 0, temp;
-    cin >> first; 
+    int numberOfInts = stoi(argv[1]), temp; 
     for(int i = 0; i < numberOfInts; i ++){
         cin >> temp;        
         ternary->insert(ternary->getRoot(),temp);
